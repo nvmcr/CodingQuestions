daaef7cdcd18755f9d5a228353f077e8bf6bcea9
Megha Chandra Nandyala <meghachandrareddy@gmail.com>
2023-01-21 15:48:27 -0800

    Update buy_sell_stock.py

--- 121.Best_Time_to_buy_and_sell_stock/buy_sell_stock.py
+++ 121.Best_Time_to_buy_and_sell_stock/buy_sell_stock.py
@@ -2,11 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_buy = float('inf')
         max_prof = 0
-        for price in prices:
+        for i in range(len(prices)):
             #Alwaystry to buy the cheapest
-            if price < min_buy:
-                min_buy = price
+            if prices[i] < min_buy:
+                min_buy = prices[i]
             #If we already have cheapest, go for max profit
-            elif price - min_buy > max_prof:
-                max_prof = price - min_buy
+            elif prices[i] - min_buy > max_prof:
+                max_prof = prices[i] - min_buy
         return max_prof
