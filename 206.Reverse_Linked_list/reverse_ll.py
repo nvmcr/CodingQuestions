086831159b1074f46aa0eb73f4967375a1095844
Megha Chandra Nandyala <meghachandrareddy@gmail.com>
2023-01-22 10:48:27 -0800

    Update reverse_ll.py

--- 206.Reverse_Linked_list/reverse_ll.py
+++ 206.Reverse_Linked_list/reverse_ll.py
@@ -14,3 +14,13 @@ class Solution:
             curr_node = next_node
         head = curr_node
         return prev_node
+```
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return head #Empty.
+        if not head.next: return head #We reached end.
+        orig_head = self.reverseList(head.next) #Traverse to end, orig_head is now end node.
+        head.next.next = head #Swap head with right node.
+        head.next = None #So we don't wind up in infinite loop.
+        return orig_head #Very last thing returned. End node!
+    ```
