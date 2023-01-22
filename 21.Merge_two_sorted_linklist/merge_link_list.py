216dfcf56bf83598b47ba61d058d52df121d58b5
Megha Chandra Nandyala <meghachandrareddy@gmail.com>
2023-01-22 12:26:19 -0800

    Create merge_link_list.py

---
+++ 21.Merge_two_sorted_linklist/merge_link_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:#if one list is completed, just return the other list
+            return list2 or list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
