138d3f4ca7a5a0090e8d088b7f6487615ba283a2
Megha Chandra Nandyala <meghachandrareddy@gmail.com>
2023-04-05 16:48:39 -0700

    Update nearest_exit_matrix.py

--- 1926.Nearest_Exit_from_Entrance_in_Matrix/nearest_exit_matrix.py
+++ 1926.Nearest_Exit_from_Entrance_in_Matrix/nearest_exit_matrix.py
@@ -2,6 +2,27 @@ class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         rows, cols = len(maze), len(maze[0])
         er, ec = entrance
+        q = deque([(er,ec,0)])
+        #In place
+        maze[er][ec] = '+' #marking as visited
+        direct = [[0,1],[0,-1],[1,0],[-1,0]]
+
+        while q:
+            r, c, d = q.popleft()
+
+            for dr, dc in direct:
+                newr, newc = r+dr, c+dc
+                if 0<=newr<rows and 0<=newc<cols and maze[newr][newc] == '.':
+                    if (newr == 0 or newr==rows-1 or newc==0 or newc==cols-1): #If on border
+                        return d+1
+                    q.append([newr,newc,d+1])
+                    maze[newr][newc] = '+'
+        
+        return -1
+
+        ''' O(m.n) space
+        rows, cols = len(maze), len(maze[0])
+        er, ec = entrance
         q = deque([(er,ec,0)]) #distance is 0
         visit = set((er,ec))
         direct = [[0,1],[0,-1],[1,0],[-1,0]]
@@ -19,4 +40,4 @@ class Solution:
                     visit.add((newr,newc))
         
         return -1
-        
+        '''
